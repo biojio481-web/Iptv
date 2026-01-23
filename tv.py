d673254acb22dd3659712e5c271259569f0379c3
biojio481-web <biojio481@gmail.com>
2026-01-23 18:15:27 +0600

    Update tv.py

--- tv.py
+++ tv.py
@@ -1,24 +1,24 @@
 import datetime
 import pytz
 
-# বাংলাদেশ সময় অনুযায়ী জোন সেট করা
+# Bangladesh er timezone set kora
 tz = pytz.timezone('Asia/Dhaka')
 now = datetime.datetime.now(tz)
 hour = now.hour
 
-# আপনার শিডিউল অনুযায়ী লিঙ্ক
-if 10 <= hour < 18:  # সকাল ১০টা - সন্ধ্যা ৬টা (Cartoon)
+# Schedule logic
+if 10 <= hour < 18:  # Shokal 10ta theke Shondha 6ta
     url = "https://ranapk.online/RANAPK33x/TVD/play.php?id=734308"
-    title = "Cartoon 24/7"
-elif 18 <= hour < 24: # সন্ধ্যা ৬টা - রাত ১২টা (Sports)
+    title = "Cartoon Live 24/7"
+elif 18 <= hour < 24: # Shondha 6ta theke Raat 12ta
     url = "http://172.16.29.34/live/ontest1/ontest1/461.m3u8"
-    title = "Sports 24/7"
-else: # রাত ১২টা - সকাল ১০টা (Entertainment)
+    title = "Sports Live 24/7"
+else: # Raat 12ta theke Shokal 10ta
     url = "https://ranapk.online/RANAPK33x/TVD/play.php?id=734323"
-    title = "Entertainment 24/7"
+    title = "Entertainment Live 24/7"
 
-# প্লেলিস্ট ফাইল তৈরি করা
+# File update kora
 with open('main.m3u', 'w') as f:
     f.write(f'#EXTM3U\n#EXTINF:-1, {title}\n{url}')
 
-print(f"Update done! Current: {title}")
+print(f"Update done! Current Bangladesh Hour: {hour}, Playing: {title}")
