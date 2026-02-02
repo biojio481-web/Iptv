49ac9772be1027143a9e5a9947edfec256278453
biojio481-web <biojio481@gmail.com>
2026-02-02 23:19:03 +0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,13 +15,13 @@ special_channels_content = f"""#EXTM3U
 #EXTINF:-1 tvg-logo="{SPECIAL_LOGO_URL}" logo="{SPECIAL_LOGO_URL}" group-title="{SPECIAL_GROUP}",Live-1
 http://172.16.29.2:8090/hls/tsportshd.m3u8
 #EXTINF:-1 tvg-logo="{SPECIAL_LOGO_URL}" logo="{SPECIAL_LOGO_URL}" group-title="{SPECIAL_GROUP}",Live-2
-http://30.30.30.30:8088/101/index.m3u8?token=9b9a5c6dae6851f8da3d7d817417bd3c5e3ebb0d-e8ee13fe456593685a17721b2b6f21eb-1769449109-1769445509
+http://30.30.30.30:8088/128/index.m3u8?token=379b3b2022f7fdc86ebe098087328d44d6825a26-be680193a029e341c348a08f99b9a195-1770055258-1770051658
 #EXTINF:-1 tvg-logo="{SPECIAL_LOGO_URL}" logo="{SPECIAL_LOGO_URL}" group-title="{SPECIAL_GROUP}",Live-3
-http://172.16.29.2:8090/hls/ASportsHD.m3u8
+http://172.16.29.2:8090/hls/StarSports1HD.m3u8
 #EXTINF:-1 tvg-logo="{SPECIAL_LOGO_URL}" logo="{SPECIAL_LOGO_URL}" group-title="{SPECIAL_GROUP}",Live-4
 http://172.16.29.34/live/ontest1/ontest1/347.m3u8
 #EXTINF:-1 tvg-logo="{SPECIAL_LOGO_URL}" logo="{SPECIAL_LOGO_URL}" group-title="{SPECIAL_GROUP}",Live-5
-https://ranapk.online/OPPLEX/RANAPK8/play.php?id=167583
+https://ranapk.online/OPPLEX/RANAPK8/play.php?id=167594
 #EXTINF:-1 tvg-logo="{SPECIAL_LOGO_URL}" logo="{SPECIAL_LOGO_URL}" group-title="{SPECIAL_GROUP}",Live-6
 https://ranapk.online/OPPLEX/RANAPK8/play.php?id=109987
 """
