c68d0ca840210dc11f6b0fb128a9cb7d6a6da1e7
biojio481-web <biojio481@gmail.com>
2026-02-05 17:25:30 +0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,7 +15,7 @@ special_channels_content = f"""#EXTM3U
 #EXTINF:-1 tvg-logo="{SPECIAL_LOGO_URL}" logo="{SPECIAL_LOGO_URL}" group-title="{SPECIAL_GROUP}",Live-1
 http://172.16.29.2:8090/hls/tsportshd.m3u8
 #EXTINF:-1 tvg-logo="{SPECIAL_LOGO_URL}" logo="{SPECIAL_LOGO_URL}" group-title="{SPECIAL_GROUP}",Live-2
-http://30.30.30.30:8088/128/index.m3u8?token=379b3b2022f7fdc86ebe098087328d44d6825a26-be680193a029e341c348a08f99b9a195-1770055258-1770051658
+http://30.30.30.30:8088/110/index.m3u8?token=2057b0d0758dbd40522cecd5c68ef66bc6a50c8b-e49de3276d778b87b692635cacf64bfb-1770292417-1770288817
 #EXTINF:-1 tvg-logo="{SPECIAL_LOGO_URL}" logo="{SPECIAL_LOGO_URL}" group-title="{SPECIAL_GROUP}",Live-3
 http://172.16.29.2:8090/hls/StarSports1HD.m3u8
 #EXTINF:-1 tvg-logo="{SPECIAL_LOGO_URL}" logo="{SPECIAL_LOGO_URL}" group-title="{SPECIAL_GROUP}",Live-4
