65b87100bf4b0a3ef19700391e3f8a75941ea931
biojio481-web <biojio481@gmail.com>
2026-05-24 16:01:08 +0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,9 +1,4 @@
 #EXTM3U
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)
-
-:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:
-                     MAIN LIVE CHANNELS
-:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:
 
 #EXTINF:-1 tvg-id="live1" tvg-name="Live 1" tvg-logo="https://i.postimg.cc/2jczw2z4/file-000000009b507209933f01562a8e146a.png" group-title="My Live",Live 1
 https://tvsen5.aynaott.com/willowhd/index.m3u8
@@ -13,11 +8,6 @@ https://tvsen7.aynaott.com/tsports-hd/index.m3u8
 
 #EXTINF:-1 tvg-id="live3" tvg-name="Live 3" tvg-logo="https://i.postimg.cc/2jczw2z4/file-000000009b507209933f01562a8e146a.png" group-title="My Live",Live 3
 https://tvsen6.aynaott.com/asports/tracks-v1a1/mono.ts.m3u8
-
-
-:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:
-                     EXTERNAL PLAYLIST SECTION
-:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:
 
 #EXTINF:-1 tvg-id="external_list" tvg-name="External Playlist" group-title="External",Arif External List
 https://raw.githubusercontent.com/mdarif2743/sky-life/refs/heads/main/Arif.m3u
