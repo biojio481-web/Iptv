4f1ce79cb6af0b93f06f2a8f40ba84f4c1484660
biojio481-web <biojio481@gmail.com>
2026-07-22 00:09:41 +0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import requests
 # =================================================================
 
 LIVE_1_LINK = "https://ranapkbd.site/RANAPK33g/TVD/play.php?id=661053"
-LIVE_2_LINK = "https://ranapkbd.site/RANAPK33g/TVD/play.php?id=809398"
+LIVE_2_LINK = "https://ranapkz.site/RANAPK33k/TVD/play.php?id=944612"
 LIVE_3_LINK = "https://ranapkz.site/RANAPK33k/TVD/play.php?id=372986"
 
 # ⚽ FIFA World Cup 2026 চ্যানেলগুলোর লিংক (এখানে আপনার লিংক বসিয়ে দিন)
